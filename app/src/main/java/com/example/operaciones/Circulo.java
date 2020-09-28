489f4a3b2466c39ef37a015eff56c3cfea27fd4c
andres pena sierra <apena19@andress-MacBook-Pro.local>
2020-09-28 18:04:21 -0500

    area completed

--- app/src/main/java/com/example/operaciones/Circulo.java
+++ app/src/main/java/com/example/operaciones/Circulo.java
@@ -3,12 +3,35 @@ package com.example.operaciones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Math.PI;
 
 public class Circulo extends AppCompatActivity {
-
+    private EditText radio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circulo);
+        radio = findViewById(R.id.txtRadio);
+    }
+    public void Calcular(View v){
+        Double resultado;
+        String datos, nombre_operacion, radioC ;
+        Operacion op;
+        nombre_operacion = getString(R.string.lbl_area_circulo);
+        radioC = radio.getText().toString();
+        datos = getString(R.string.radio) + " " + radioC  ;
+        resultado =  PI * ( parseDouble(radioC ) * parseDouble(radioC ) ) ;
+        op = new Operacion(nombre_operacion, datos, resultado );
+        op.guardar();
+        Toast.makeText(this, getString(R.string.mensaje_area) + " " +resultado,Toast.LENGTH_LONG).show();
+    }
+    public void limpiar(View v){
+        radio.setText("");
+        radio.requestFocus();
     }
 }
--- app/src/main/java/com/example/operaciones/Triangulo.java
+++ app/src/main/java/com/example/operaciones/Triangulo.java
@@ -3,12 +3,37 @@ package com.example.operaciones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.lang.Double.parseDouble;
 
 public class Triangulo extends AppCompatActivity {
-
+    private EditText base,altura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triangulo);
+        base = findViewById(R.id.txtBase);
+        altura = findViewById(R.id.txtAltura);
+    }
+    public void Calcular(View v){
+        Double resultado;
+        String datos, nombre_operacion, baseT, alturaT ;
+        Operacion op;
+        nombre_operacion = getString(R.string.lbl_areatriangulo);
+        baseT = base.getText().toString();
+        alturaT = altura.getText().toString();
+        datos = getString(R.string.lblbase) + " " + baseT + "--" + getString(R.string.lbl_altura) + " " + alturaT ;
+        resultado =  ( parseDouble(baseT ) * parseDouble(alturaT) ) / 2;
+        op = new Operacion(nombre_operacion, datos, resultado );
+        op.guardar();
+        Toast.makeText(this, getString(R.string.mensaje_area) + " " +resultado,Toast.LENGTH_LONG).show();
+    }
+    public void limpiar(View v){
+        base.setText("");
+        altura.setText("");
+        base.requestFocus();
     }
 }
